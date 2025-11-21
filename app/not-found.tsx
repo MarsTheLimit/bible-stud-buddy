12cf8813b60100a4b5449fede54538ae4ce6e3e7
Bo Vyper <93620565+MarsTheLimit@users.noreply.github.com>
2025-11-21 12:27:15 -0600

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="card-body p-5 bg bg-gradient text-center">
+      <h2 className="text-gradient display-4 fw-bold mb-3">404 - Page Not Found</h2>
+      <p>Sorry, we couldn&apos;t find the page you were looking for.</p>
+    </div>
+  );
+}
